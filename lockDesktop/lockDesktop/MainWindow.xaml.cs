4536be52b8965923d641b0292a9b33de05bf9989
arkadoel <fer.d.minguela@gmail.com>
2020-01-15 08:51:18 +0100

    lock session

---
+++ lockDesktop/lockDesktop/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lockDesktop
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPass.Password == "")
+            {
+                this.Close();
+            }
+        }
+        
+       
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+        }
+
+    }
+}
